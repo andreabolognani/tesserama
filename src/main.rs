74aae41d0109a802d608d6d4057e53af845cdcf6
Andrea Bolognani <eof@kiyuko.org>
2017-05-12 00:28:18 +0200

    Smallest viable gtk::Application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate gio;
+extern crate gtk;
+
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+	let argc: i32 = 0;
+	let argv: &[&str] = &[];
+
+	let app = gtk::Application::new(None, gio::APPLICATION_FLAGS_NONE).unwrap();
+	app.connect_activate(|_| {});
+	app.run(argc, argv);
 }
