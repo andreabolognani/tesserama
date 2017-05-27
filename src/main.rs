3c69e16cc70cde40a93b1dfca3bc9d0781620ada
Andrea Bolognani <eof@kiyuko.org>
2017-05-28 01:47:46 +0200

    Add window to application

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,48 @@ extern crate gtk;
 
 use gtk::prelude::*;
 
-struct Tesserama {
-	app: gtk::Application,
-}
-
-impl Tesserama {
-	fn new() -> Result<Self, ()> {
-		let flags = gio::ApplicationFlags::empty();
-		match gtk::Application::new(None, flags) {
-			Ok(app) => {
-				app.connect_activate(|_| {});
-				Ok(Tesserama { app: app })
-			}
-			Err(_) => Err(())
+fn app_new() -> gtk::Application {
+	let flags = gio::ApplicationFlags::empty();
+	match gtk::Application::new(None, flags) {
+		Ok(app) => {
+			let app_clone = app.clone();
+			app.connect_activate(move |_| {
+				app_activate(&app_clone);
+			});
+			app
+		}
+		Err(_) => {
+			panic!("GTK+ initialization error");
 		}
 	}
+}
 
-	fn run(&self) -> i32 {
-		let args: Vec<String> = std::env::args().collect();
-		let args: Vec<&str> = args.iter().map(|s| s.as_ref()).collect();
+fn app_activate(app: &gtk::Application) {
+		let win = win_new(app);
+		win_show(&win);
+}
 
-		let argc: i32 = args.len() as i32;
-		let argv: &[&str] = &args;
+fn app_run(app: &gtk::Application) {
+	let args: Vec<String> = std::env::args().collect();
+	let args: Vec<&str> = args.iter().map(|s| s.as_ref()).collect();
 
-		self.app.run(argc, argv)
-	}
+	let argc: i32 = args.len() as i32;
+	let argv: &[&str] = &args;
+
+	app.run(argc, argv);
+}
+
+fn win_new(app: &gtk::Application) -> gtk::ApplicationWindow {
+	let win = gtk::ApplicationWindow::new(app);
+	win.set_title("Tesserama");
+	win
+}
+
+fn win_show(win: &gtk::ApplicationWindow) {
+	win.show_all();
 }
 
 fn main() {
-	match Tesserama::new() {
-		Ok(value) => {
-			value.run();
-		}
-		Err(_) => {
-			println!("GTK+ initialization error");
-		}
-	}
+	let app = app_new();
+	app_run(&app);
 }
