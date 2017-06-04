b0ed38bf4678cd9be18e07f04d53a76cc2ab8b9b
Andrea Bolognani <eof@kiyuko.org>
2017-06-04 17:49:01 +0200

    Add and hook up actions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate glib;
 extern crate gio;
 extern crate gtk;
 
@@ -20,6 +21,11 @@ impl Application {
 	}
 
 	fn setup(&self) {
+		self.parent.set_accels_for_action("win.search", &["<Ctrl>f"]);
+		self.parent.set_accels_for_action("win.insert", &["<Ctrl>i"]);
+		self.parent.set_accels_for_action("win.open", &["<Ctrl>o"]);
+		self.parent.set_accels_for_action("win.save", &["<Ctrl>s"]);
+
 		let _self = self.clone();
 		self.parent.connect_activate(move |_| {
 			_self.activate_action();
@@ -52,6 +58,11 @@ struct Window {
 	searchbutton: gtk::ToggleButton,
 	insertbutton: gtk::Button,
 	menubutton: gtk::ToggleButton,
+	searchaction: gio::SimpleAction,
+	insertaction: gio::SimpleAction,
+	menuaction: gio::SimpleAction,
+	openaction: gio::SimpleAction,
+	saveaction: gio::SimpleAction,
 }
 
 impl Window {
@@ -62,6 +73,19 @@ impl Window {
 			searchbutton: gtk::ToggleButton::new(),
 			insertbutton: gtk::Button::new_with_label("Insert"),
 			menubutton: gtk::ToggleButton::new(),
+			searchaction: gio::SimpleAction::new_stateful(
+				"search",
+				None,
+				&false.to_variant(),
+			),
+			insertaction: gio::SimpleAction::new("insert", None),
+			menuaction: gio::SimpleAction::new_stateful(
+				"menu",
+				None,
+				&false.to_variant(),
+			),
+			openaction: gio::SimpleAction::new("open", None),
+			saveaction: gio::SimpleAction::new("save", None),
 		};
 		ret.setup();
 		ret
@@ -70,6 +94,43 @@ impl Window {
 	fn setup(&self) {
 		self.parent.set_title("Tesserama");
 		self.parent.set_default_size(800, 600);
+
+		/* Actions */
+
+		let _self = self.clone();
+		self.searchaction.connect_activate(move |_,_| {
+			_self.search_action_activated();
+		});
+		self.searchaction.set_enabled(false);
+		self.parent.add_action(&self.searchaction);
+
+		let _self = self.clone();
+		self.insertaction.connect_activate(move |_,_| {
+			_self.insert_action_activated();
+		});
+		self.insertaction.set_enabled(false);
+		self.parent.add_action(&self.insertaction);
+
+		let _self = self.clone();
+		self.menuaction.connect_activate(move |_,_| {
+			_self.menu_action_activated();
+		});
+		self.parent.add_action(&self.menuaction);
+
+		let _self = self.clone();
+		self.openaction.connect_activate(move |_,_| {
+			_self.open_action_activated();
+		});
+		self.parent.add_action(&self.openaction);
+
+		let _self = self.clone();
+		self.saveaction.connect_activate(move |_,_| {
+			_self.save_action_activated();
+		});
+		self.saveaction.set_enabled(false);
+		self.parent.add_action(&self.saveaction);
+
+		/* Header bar */
 
 		self.headerbar.set_show_close_button(true);
 		self.parent.set_titlebar(&self.headerbar);
@@ -98,6 +159,21 @@ impl Window {
 
 	fn show_all(&self) {
 		self.parent.show_all();
+	}
+
+	fn search_action_activated(&self) {
+	}
+
+	fn insert_action_activated(&self) {
+	}
+
+	fn menu_action_activated(&self) {
+	}
+
+	fn open_action_activated(&self) {
+	}
+
+	fn save_action_activated(&self) {
 	}
 }
 
